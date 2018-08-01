47c8af725ebdaba3ba7f57928987ce9b6fdff32a
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-01 18:51:47 -0500

    minor

--- lib/Compose.py
+++ lib/Compose.py
@@ -6,15 +6,26 @@ class Compose(object):
     _app_id = None
     _app_secret = None
     _base_url = "%s/web-api/%s/%s/%s/json?app_id=%s&app_secret=%s&%s"
+    _services = []
+    initialized = False
 
     def __init__(self, host, app_id, app_secret, version='1.0'):
         self._host = host[:-1] if host[-1] == '/' else host
         self._version = version
         self._app_id = app_id
         self._app_secret = app_secret
-        # get API info
+        # load available endpoints
+        if not self.reload_endpoints(): return
+        # set as initialized
+        self.initialized = True
+
+    def reload_endpoints(self):
+        # remove all services
+        for service_name in self._services:
+            self.__dict__.pop(service_name, None)
+        # get new endpoints
         endpoints = self.endpoints()
-        if endpoints is None: return
+        if endpoints is None: return False
         # parse info
         for endpoint in endpoints:
             service_name = endpoint['service']
@@ -23,6 +34,7 @@ class Compose(object):
             self._register_service(service_name)
             # create action proxy
             self.__dict__[service_name]._register_action(action_name, endpoint)
+        return True
 
     def endpoints(self):
         success, data, msg = self._get('api', 'app_info')
@@ -31,10 +43,16 @@ class Compose(object):
             return None
         return data['endpoints']
 
+    def is_endpoint_available(self, endpoint):
+        endpoints = self.endpoints()
+        if endpoints is None: return False
+        return endpoint in [ e['endpoint'] for e in endpoints ]
+
     def _register_service(self, service_name):
         # create service proxy if it does not exist
         if service_name not in self.__dict__:
             self.__dict__[service_name] = ComposeServiceProxy(self, service_name)
+            self._services.append( service_name )
 
     def _get(self, service, action, arguments={}):
         url = self._build_url(service, action, arguments)
@@ -43,7 +61,7 @@ class Compose(object):
         # return result
         if res['code'] == 200:
             return True, res['data'], 'OK'
-        return False, res['message'], None
+        return False, None, res['message']
 
     def _build_url(self, service, action, arguments):
         return self._base_url % (
