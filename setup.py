5f6783b9f3e32b09c9a4666afba2461281237aeb
Andrea F. Daniele <afdaniele@ttic.edu>
2020-05-15 19:20:33 -0500

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 # :==> Fill in your project data here
-version = '0.1.4'
+version = '0.1.5'
 library_name = 'compose_cms'
 library_webpage = 'https://github.com/afdaniele/compose-python'
 maintainer = 'Andrea F. Daniele'
