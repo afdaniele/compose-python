84a8d1b3c0929e858d90843e85fadbb3d83fde6b
Andrea F. Daniele <afdaniele@duckietown.com>
2023-02-02 15:13:50 -0500

    added __version__

--- include/compose_cms/__init__.py
+++ include/compose_cms/__init__.py
@@ -3,3 +3,6 @@ from .compose import Compose, ComposePackage, ComposePage, ComposeDatabase, \
     ComposePackageConfiguration
 from .restapi import ComposeRestAPI
 from .exceptions import APIError
+
+
+__version__ = "1.0.4"
